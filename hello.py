321d6472d59fdb5f0118eeaa5f28ea0550071f5c
ElenaI.github.io.git <epodd13@hotmail.com>
2017-08-21 18:13:51 +0300

    f

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-def hello_app(environ, start_response):
-    body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+def app(environ, start_response):
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     response_string = [('Content-type', 'text/plain')]
     start_response('200 OK', response_string)
     return body
