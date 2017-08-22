be85e3bf3ad7c62f8f25d95d8b296c08405b601b
ElenaI.github.io.git <epodd13@hotmail.com>
2017-08-22 17:26:29 +0300

    f

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx start
-
+sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn start
