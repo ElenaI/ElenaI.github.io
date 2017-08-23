f7b1cd19c6f09a2804739b4b74127c735e86517a
ElenaI.github.io.git <epodd13@hotmail.com>
2017-08-23 14:01:41 +0300

    f

--- etc/hello.py
+++
@@ -1,5 +0,0 @@
-def app(environ, start_response):
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    response_string = [('Content-type', 'text/plain')]
-    start_response('200 OK', response_string)
-    return body
