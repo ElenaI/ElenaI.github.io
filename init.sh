d3c5cd506ce1a71fe1fda6f9b2efef6a7c598607
ElenaI.github.io.git <epodd13@hotmail.com>
2017-08-25 15:53:39 +0300

    f

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo rm /etc/gunicorn/test
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #gunicorn -c /etc/gunicorn.d/test ask.wsgi:application
-cd /home/box/web/ask
-sudo gunicorn --bind 0.0.0.0:8000 --access-logfile acc.log --error-logfile err.log ask.wsgi:application &
+cd /home/box/web/ask/
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
