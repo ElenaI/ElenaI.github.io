831fb822f1768e7cc7cbec127a4e711d9970d730
ElenaI.github.io.git <epodd13@hotmail.com>
2017-08-29 15:07:34 +0300

    f

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,12 +1,16 @@
-from django.conf.urls import url,patterns
+from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = patterns('qa.views',
+admin.autodiscover()
+
+
+urlpatterns = [
     url(r'^$', 'test'),
-    url(r'^login/.*$', 'test',name = 'login'),
-    url(r'^signup/.*', 'test',name = 'signup'),
-    url(r'^question/(?P<id>[0-9]+)/$', 'test',name = 'question'),
-    url(r'^ask/.*', 'test',name = 'ask'),
-    url(r'^popular/.*', 'test',name = 'popular'),
-    url(r'^new/.*', 'test',name = 'new'),
-)
+    url(r'^login/.*$', qa.views.test,name = 'login'),
+    url(r'^signup/.*', qa.views.test,name = 'signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', qa.views.test,name = 'question'),
+    url(r'^ask/.*', qa.views.test,name = 'ask'),
+    url(r'^popular/.*', qa.views.test,name = 'popular'),
+    url(r'^new/.*', qa.views.test,name = 'new'),
+]
+
