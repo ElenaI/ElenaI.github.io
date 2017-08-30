545d5e621fb1eadf1850ccf4012d734704875eab
ElenaI.github.io.git <epodd13@hotmail.com>
2017-08-30 15:23:29 +0300

    f

--- init.sh
+++ init.sh
@@ -3,8 +3,9 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test_wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test_django
-sudo /etc/init.d/gunicorn restart
-#gunicorn -c /etc/gunicorn.d/test ask.wsgi:application
-
-#cd /home/box/web/ask/
-#sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+#sudo /etc/init.d/gunicorn restart
+#gunicorn -c /etc/gunicorn.d/test_django ask.wsgi:application
+cd /home/box/web/ask/
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+cd /home/box/web
+sudo gunicorn -b 0.0.0.0:8080 hello:web &
