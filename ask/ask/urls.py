bee74b8665cdbf28f23c0eee138ee6245dd7353e
ElenaI.github.io.git <epodd13@hotmail.com>
2017-08-30 12:26:47 +0300

    f

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -6,12 +6,12 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^$', qa.views.test),
-    url(r'^login/.*$', test,name = 'login'),
-    url(r'^signup/.*', name = 'signup'),
-    url(r'^question/(?P<id>[0-9]+)/$', test,name = 'question'),
-    url(r'^ask/.*', test,name = 'ask'),
-    url(r'^popular/.*', test,name = 'popular'),
+    url(r'^$',test),
+    url(r'^login/.*$',test,name = 'login'),
+    url(r'^signup/.*',test, name = 'signup'),
+    url(r'^question/(?P<id>[0-9]+)/$',test,name = 'question'),
+    url(r'^ask/.*',test,name = 'ask'),
+    url(r'^popular/.*',test,name = 'popular'),
     url(r'^new/.*', test,name = 'new'),
 ]
 
