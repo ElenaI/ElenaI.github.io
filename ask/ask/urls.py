39dfe3db2a037c5a84642834c2e0707482fde7be
ElenaI.github.io.git <epodd13@hotmail.com>
2017-09-06 13:57:19 +0300

    f

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,15 +1,27 @@
-from django.conf.urls import url
-#from django.contrib import admin
-from qa.views import test
+from django.conf.urls import patterns, url
 
-#admin.autodiscover()
-urlpatterns = [
-    url(r'^$',test),
-    url(r'^login/.*$',login, {'template_name': 'qa/login.html'}, name='login',name = 'login'),
-    url(r'^signup/.*',signup, name = 'signup'),
-    url(r'^question/(?P<id>[0-9]+)/$',question_detail,name = 'question_detail'),
-    url(r'^ask/.*',ask,name = 'ask'),
-    url(r'^popular/.*',popular_question,name = 'popular'),
-    url(r'^new/.*', test,name = 'new'),
-]
+from django.contrib import admin
+from django.contrib.auth.views import login
+admin.autodiscover()
+from qa.views import (
+    test, new_questions, popular_questions, question_detail, ask, answer,
+    signup)
 
+
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', test, name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^new/$', test, name='new'),
+    url(r'^popular/$', popular_questions, name='popular'),
+    url(r'^ask/', ask, name='ask'),
+    url(r'^answer/', answer, name='answer'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', question_detail,
+        name='question_detail'),
+    url(r'^signup/$', signup, name='signup'),
+    url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^$', new_questions, name='index'),
+)
